bdc5bca7dc1b25cbf6c5104fd09361651c599e38
Daichi HAYASHI <daichikuwa0618@gmail.com>
2019-06-17 12:54:31 +0900

    変数のモジュール化 sub. grid, init の追加

--- 01/01.f90
+++ 01/01.f90
@@ -1,7 +1,7 @@
 ! ======================================================================
 !     数値流体ゼミ 課題 1
 !     波動方程式を数値的に解く.
-!     
+!
 !     ∂u/∂t + c*∂u/∂x = 0 eq:2.6
 !     c    = 1
 !     CFL  = 0.5
@@ -12,7 +12,7 @@
 !     B.C.
 !     u[0]      = u[1]    : left boundary
 !     u[imax+1] = u[imax] : right boundary
-!     
+!
 !     numerical flux 数値流速 : 1 次精度風上差分
 !     ~f_i+1/2 = 1/2{(f_i+1 + f_i) - |c|(u_i+1 - u_i)} eq:2.51
 !
@@ -29,49 +29,92 @@
 !     7. 終了
 ! ======================================================================
 
-program myCFD01
-
-implicit none
+! =====================
+! module
+! =====================
+module myMod
+    implicit none
     ! Note: change 13 -> 5 to make everything single precision
     integer , parameter :: p2 = selected_real_kind(13) ! Double Precision
     real(p2), parameter :: zero = 0.0_p2
     real(p2), parameter ::  one = 1.0_p2
     real(p2), parameter :: half = 0.5_p2
 
-! セルデータの構造体
-type cell_data
-    real(p2) :: xc     ! Cell-center coordinate
-    real(p2) :: u(1)   ! Conservative variable
-    real(p2) :: u0(1)  ! previous time step value
-    real(p2) :: res(1) ! Residual = f_{j+1/2) - f_{j-1/2)
-end type cell_data
+    ! セルデータの構造体
+    type cell_data
+        real(p2) :: xg     ! Cell-center coordinate
+        real(p2) :: u(1)   ! Conservative variable
+        real(p2) :: u0(1)  ! previous time step value
+        real(p2) :: res(1) ! Residual = f_{j+1/2) - f_{j-1/2)
+    end type cell_data
 
-! Local variables
-type(cell_data), allocatable :: cell(:) ! Array of cell-data
-real(p2)                     :: xmin, xmax ! 領域の端
-real(p2)                     :: dx         ! セル幅
-real(p2)                     :: t, tf      ! 現在と最後の時間
-real(p2)                     :: cfl, dt    ! CFL 数と時間刻み幅
-integer                      :: ncells     ! セル数
-integer                      :: nsteps     ! 時間ステップ数
-integer                      :: itime      ! 時間ステップのインデックス
-integer                      :: i, j       ! ループで使う
+    ! Local variables
+    type(cell_data), allocatable :: cell(:) ! Array of cell-data
+    real(p2)                     :: xmin, xmax ! 領域の端
+    real(p2)                     :: dx         ! セル幅
+    real(p2)                     :: t, tf      ! 現在と最後の時間
+    real(p2)                     :: cfl, dt    ! CFL 数と時間刻み幅
+    integer                      :: ncells     ! セル数
+    integer                      :: nsteps     ! 時間ステップ数
+    integer                      :: itime      ! 時間ステップのインデックス
+    integer                      :: i, j       ! ループで使う
+end module myMod
 
 ! ======================================================================
-! main function
+! main
 ! ======================================================================
-! Input params
-call param
-! I.C.
-call init
+program main
+    use myMod
+    implicit none
 
+    ! Input params
+    call param
+    ! grid
+    call grid
+    ! I.C.
+    call init
+
+end program main
+! ======================================================================
+! main end
+! ======================================================================
 
 ! ===============================================
 ! Input parameters and I.C.
+! ===============================================
 subroutine param
+    use myMod
     ! parameters
     ncells = 100    ! numbers of cells
     tf     = 1.0_p2 ! Final time
     cfl    = 0.5    ! CFL number
     xmin   = zero   ! Left boundary coordinate
     xmax   = one    ! Right boundary coordinate
+
+    ! ゴーストセルも含めて allocate する
+    allocate(cell(0:ncells+1))
+
+    ! セル幅
+    dx = (xmax-xmin)/real(ncells)
+end subroutine param
+
+! ===============================================
+! generate grid
+! ===============================================
+subroutine grid
+    use myMod
+    do i = 0, ncells+1
+        cell(i)%xc = xmin+real(i-1)*dx
+    end do
+end subroutine grid
+
+
+! ===============================================
+! Initial Condition
+! ===============================================
+subroutine init
+    use myMod
+    do i = 0, ncells+1
+        if(cell(i)%)
+    end do
+end subroutine init
